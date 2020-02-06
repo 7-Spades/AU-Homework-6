73d8f532a711e516ffbff4511d8ef0ac2df08259
7-Spades <edwin5106@gmail.com>
2020-02-05 20:59:17 -0600

    added history list functionality

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 console.log($);
 $(document).ready(function(){
-    var apikey= "38b878c8ec008be7efd106fd4221e555"
-    var localarea= $("#local").val()
+    var apikey= "38b878c8ec008be7efd106fd4221e555";
+    var localarea= $("#local").val();
     var Hweather= "https://api.openweathermap.org/data/2.5/weather?q=Houston&units=imperial&appid=" + apikey;
     var Hforcast= "https://api.openweathermap.org/data/2.5/forecast?q=Houston&units=imperial&appid=" + apikey;
-    var Dweather= "https://api.openweathermap.org/data/2.5/weather?q=" + localarea + "&units=imperial&appid=" + apikey;
-    var Dforcast= "https://api.openweathermap.org/data/2.5/forecast?q=" + localarea + "&units=imperial&appid=" + apikey;
-    
+    var Dweather= "https://api.openweathermap.org/data/2.5/weather?q=";
+    var Dforcast= "https://api.openweathermap.org/data/2.5/forecast?q=";
+    var City= $("#city");
+    var tem= $("#temp");
+    var hum= $("#humid");
+    var win= $("#wind");
 
     $.ajax({
         url: Hweather,
         method: "GET"
     }).then(function(response){
         console.log(response);
-        $("#city").text(response.name);
-        $("#temp").text("Temperature: " + response.main.temp.toFixed() + "F");
-        $("#humid").text("Humidity: " + response.main.humidity + "%");
-        $("#wind").text("Wind Speed: " + response.wind.speed.toFixed() + " m/h");
+        City.text(response.name);
+        tem.text("Temperature: " + response.main.temp.toFixed() + "F");
+        hum.text("Humidity: " + response.main.humidity + "%");
+        win.text("Wind Speed: " + response.wind.speed.toFixed() + " m/h");
     })
 
     $.ajax({
@@ -32,5 +35,40 @@ $(document).ready(function(){
         };
     })
 
+    $(".list-group-item").on({mouseover: function(){
+        $(this).attr("class", "list-group-item active")},
+        mouseleave: function(){
+            $(this).attr("class", "list-group-item")
+        }
+    });
     
+    $(".list-group-item").on("click", function(){
+        console.log($(this).attr("data-city"));
+        var town= $(this).attr("data-city");
+
+    $.ajax({
+        url: Dweather + town + "&units=imperial&appid=" + apikey,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        City.text(response.name);
+        tem.text("Temperature: " + response.main.temp.toFixed() + "F");
+        hum.text("Humidity: " + response.main.humidity + "%");
+        win.text("Wind Speed: " + response.wind.speed.toFixed() + " m/h");
+    });
+
+    $.ajax({
+        url: Dforcast + town + "&units=imperial&appid=" + apikey,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var result= response.list;
+        for(var i=0; i<5; i++){
+            $("#date0"+i).text(result[8*i].dt_txt);
+            $("#temp0"+i).text("Temp: " + result[8*i].main.temp.toFixed() + "F");
+            $("#humid0"+i).text("Humid: " + result[8*i].main.humidity + "%");
+        };
+    })
+    
+})
 });
